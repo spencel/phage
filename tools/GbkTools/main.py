a04d14ef3258d7fcce8fca709c4e9b792d905797
S L <spence.lank@gmail.com>
2016-08-04 22:42:46 -0700

    commit

--- tools/GbkTools/main.py
+++ tools/GbkTools/main.py
@@ -4,9 +4,16 @@ import sys
 
 from lib.gbkParser import *
 
-#getMajorCapsid("K:/phage/dat/caudo_with_major-capsid", True)
-getMajorCapsid_ConvertToFasta("D:/PHANTOME/Viruses-caudo_with_major-capsid", True)
+command = ""
+while command != "exit":
+	command = input("Please enter a command.\n")
 
-#allGbkToJson(path="K:/PHANTOME/Viruses-caudo_with_major-capsid", debugOn=True)
-
-#gbkReport.getAllAlias(path="K:/PHANTOME/Viruses-caudo_with_major-capsid")
+	try:
+		if command == "execute":
+			#getMajorCapsid("K:/phage/dat/caudo_with_major-capsid", True)
+			getMajorCapsid_ConvertToFasta("D:/PHANTOME/Viruses-caudo_with_major-capsid", True)
+			#allGbkToJson(path="K:/PHANTOME/Viruses-caudo_with_major-capsid", debugOn=True)
+			#gbkReport.getAllAlias(path="K:/PHANTOME/Viruses-caudo_with_major-capsid")
+		break
+	except ValueError:
+		print("Error: " + ValueError)
